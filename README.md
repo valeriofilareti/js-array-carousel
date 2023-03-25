c7588779c71813510a062c23778599c1b15f2c96
valeriofilareti <valerio.filareti@gmail.com>
2023-03-25 10:43:28 +0100

    [add] counter che non funziona

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ Carosello Mono Array
 ===
 ## Consegna
 Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+
+## Svolgimento
+1) Creo un array contenente le immagini
+2) Attraverso un ciclo for, stampo in pagina tutte le immagini e gli attribuisco la classe hide
+3) Creo un HTMLCollection della classe hide
+4) Creo un contatore che aumenti in base al numero di tocchi sul tasto down e che tolga la classe hide all'elemento corrispondente all'indice dell'HTMLCollection
---
+++ js/script.js
@@ -0,0 +1,52 @@
+const images = [
+  "img/01.webp",
+  "img/02.webp",
+  "img/03.webp",
+  "img/04.webp",
+  "img/05.webp"
+]
+
+const imgWrapper = document.querySelector('.img-wrapper');
+
+const topBtn = document.getElementById('top-button')
+
+const bottomBtn = document.getElementById('bottom-button')
+
+
+
+
+
+
+
+for (i = 0; i < images.length; i++) {
+  const img = images[i];
+  imgWrapper.innerHTML += `<img class="item hide" src="${img}">`;
+  console.log(img);
+}
+
+
+
+const items = imgWrapper.getElementsByClassName('hide')
+let imgCounter = 0
+items[imgCounter].classList.remove('hide')
+
+bottomBtn.addEventListener('click', function(){
+
+  items[imgCounter].classList.add('hide');
+  imgCounter++
+  items[imgCounter].classList.remove('hide');
+
+  console.log(items[imgCounter])
+})
+
+topBtn.addEventListener('click', function(){
+  console.log('ciao')
+})
+
+
+
+
+
+
+
+
