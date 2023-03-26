f202d516a2c6c9687cf032ef3d5ede663622d8a8
valeriofilareti <valerio.filareti@gmail.com>
2023-03-26 13:07:51 +0200

    [add] bonus 2 boxes img right

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Dato un array contenente una lista di cinque immagini, creare un carosello come 
 2) Attraverso un ciclo for, stampo in pagina tutte le immagini e gli attribuisco la classe hide
 3) Creo un HTMLCollection della classe hide
 4) Creo un contatore che aumenti in base al numero di tocchi sul tasto down e che tolga la classe hide all'elemento corrispondente all'indice dell'HTMLCollection
+
+
---
+++ js/script-bonus2.js
@@ -0,0 +1,113 @@
+const images = [
+  "img/01.webp",
+  "img/02.webp",
+  "img/03.webp",
+  "img/04.webp",
+  "img/05.webp"
+]
+
+// elements
+
+const imgWrapper = document.querySelector('.img-wrapper');
+
+const topBtn = document.getElementById('top-button')
+
+const bottomBtn = document.getElementById('bottom-button')
+
+// elements
+
+
+
+// img gen
+
+for (i = 0; i < images.length; i++) {
+  const img = images[i];
+  imgWrapper.innerHTML += `<img class="item hide" src="${img}">`;
+  console.log(img);
+}
+
+// img gen
+
+
+
+
+//img counter
+
+const items = document.getElementsByClassName('item')
+
+let imgCounter = 0;
+items[imgCounter].classList.remove('hide')
+
+//img counter
+
+
+
+
+// img-control counter
+
+let imgControlCounter = 0;
+
+const imgControls = document.getElementsByClassName('cell')
+console.log(imgControls)
+for(i=0; i < imgControls.length; i++) {
+  const img = images[i];
+  imgControls[imgControlCounter].innerHTML += `<img class="item" src="${img}">`;
+  imgControlCounter++
+  console.log(imgControlCounter)
+}
+
+// img-control counter
+
+
+
+//buttons 
+
+  bottomBtn.addEventListener('click', function(){
+    if(imgCounter === 0){
+      items[imgCounter].classList.add('hide')
+      imgCounter++
+      items[imgCounter].classList.remove('hide')
+    }else if(imgCounter === 1){
+      items[imgCounter].classList.add('hide')
+      imgCounter++
+      items[imgCounter].classList.remove('hide')
+    }else if(imgCounter === 2){
+      items[imgCounter].classList.add('hide')
+      imgCounter++
+      items[imgCounter].classList.remove('hide')
+    }else if(imgCounter === 3){
+      items[imgCounter].classList.add('hide')
+      imgCounter++
+      items[imgCounter].classList.remove('hide')
+    }else if(imgCounter === 4){
+        items[imgCounter].classList.add('hide')
+        imgCounter = 0
+        items[imgCounter].classList.remove('hide')
+    }
+    console.log(imgCounter)
+  })
+  
+  topBtn.addEventListener('click', function(){
+    if(imgCounter === 0){
+      items[imgCounter].classList.add('hide')
+      imgCounter = 4
+      items[imgCounter].classList.remove('hide')
+    }else if(imgCounter === 4){
+      items[imgCounter].classList.add('hide')
+      imgCounter--
+      items[imgCounter].classList.remove('hide')
+    }else if(imgCounter === 3){
+      items[imgCounter].classList.add('hide')
+      imgCounter--
+      items[imgCounter].classList.remove('hide')
+    }else if(imgCounter === 2){
+      items[imgCounter].classList.add('hide')
+      imgCounter--
+      items[imgCounter].classList.remove('hide')
+    }else if(imgCounter === 1){
+        items[imgCounter].classList.add('hide')
+        imgCounter --
+        items[imgCounter].classList.remove('hide')
+    }
+    console.log(imgCounter)
+  })
